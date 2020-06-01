3816a82a899b3da99a4795bb4bc0192551088d17
Serdar <serdar2225@gmail.com>
2020-06-01 03:09:03 -0400

    running tests using xml

---
+++ src/Tests/Tests1.java
@@ -0,0 +1,20 @@
+package Tests;
+
+import org.testng.annotations.Test;
+
+public class Tests1 {
+    @Test
+    public void TestCase1() {
+        System.out.println("Testing test case 1");
+    }
+
+    @Test
+    public void TestCase2() {
+        System.out.println("Testing test case 2");
+    }
+
+    @Test
+    public void TestCase3() {
+        System.out.println("Testing test case 3");
+    }
+}
---
+++ src/Tests/Tests2.java
@@ -0,0 +1,20 @@
+package Tests;
+
+import org.testng.annotations.Test;
+
+public class Tests2 {
+    @Test
+    public void TestCase1() {
+        System.out.println("Testing test case 4");
+    }
+
+    @Test
+    public void TestCase2() {
+        System.out.println("Testing test case 5");
+    }
+
+    @Test
+    public void TestCase3() {
+        System.out.println("Testing test case 6");
+    }
+}
