9ce397bbc4115336c0e5a2f5ef1481e3654b280c
Serdar <serdar2225@gmail.com>
2020-06-02 01:47:04 -0400

    before group test has been udpated

--- src/Grouping/BeforeAndAfterGroupings/BeforeGroup.java
+++ src/Grouping/BeforeAndAfterGroupings/BeforeGroup.java
@@ -1,5 +1,16 @@
 package Grouping.BeforeAndAfterGroupings;
 
+import org.testng.annotations.BeforeGroups;
+
 public class BeforeGroup {
+    @BeforeGroups("regression")
+    public void beforeRegressionGroup(){
+        System.out.println("Before Regression Group");
+    }
+
+    @BeforeGroups("functional")
+    public void beforeFunctionalGroup(){
+        System.out.println("Before Functional Group");
+    }
 
 }
