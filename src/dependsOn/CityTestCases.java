53b65be966c2c71847d8b6ca6c112c20e068bb41
Serdar <serdar2225@gmail.com>
2020-06-02 20:59:12 -0400

    depends on and parameterized methods

---
+++ src/dependsOn/CityTestCases.java
@@ -0,0 +1,25 @@
+package dependsOn;
+
+import org.testng.annotations.Test;
+
+public class CityTestCases {
+    @Test(dependsOnMethods = {"login"})
+    public void create() {
+        System.out.println("City create test case");
+    }
+
+    @Test(dependsOnMethods = {"login"})
+    public void edit() {
+        System.out.println("City edit test case");
+    }
+
+    @Test(dependsOnMethods = {"login", "create"})
+    public void delete() {
+        System.out.println("City delete test case");
+    }
+
+    @Test
+    public void login() {
+        System.out.println("Login");
+    }
+}
