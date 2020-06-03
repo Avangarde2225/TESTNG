51d4ad65f8f48182a1ca53281414346c3e985ad3
Serdar <serdar2225@gmail.com>
2020-06-02 20:28:52 -0400

    include and exclude methods in xml TestNG framework

---
+++ src/Grouping/excludeAndInclude/NationlityTestCases.java
@@ -0,0 +1,20 @@
+package Grouping.excludeAndInclude;
+
+import org.testng.annotations.Test;
+
+public class NationlityTestCases {
+    @Test
+    public void create() {
+        System.out.println("Nationality create test case");
+    }
+
+    @Test
+    public void edit() {
+        System.out.println("Nationality edit test case");
+    }
+
+    @Test
+    public void delete() {
+        System.out.println("Nationality delete test case");
+    }
+}
